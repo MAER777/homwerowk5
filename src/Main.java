49008e8f30d4b9cfce5b2e2caf0e72aec02bc977
MARK777 <mark1000002@gmail.com>
2022-12-29 17:43:20 +0800

    Тест

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        test();
+        //task1();
+        //task2();
+    }
+    public static void test() {System.out.println("Тестовое");
+
     }
 }
